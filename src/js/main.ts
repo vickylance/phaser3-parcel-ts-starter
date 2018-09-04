74ee7fd66a040b5cf0fdbf504e324cbbedcc6315
Vignesh S <s.vignesh@partner.samsung.com>
2018-09-04 20:44:16 +0530

    pretty

--- src/js/main.ts
+++ src/js/main.ts
@@ -51,24 +51,22 @@ let game = new Game({
   title: 'Treasure Hunt'
 });
 
-
 function resize() {
-  var canvas = document.querySelector("canvas");
+  var canvas = document.querySelector('canvas');
   var windowWidth = window.innerWidth;
   var windowHeight = window.innerHeight;
   var windowRatio = windowWidth / windowHeight;
   var gameRatio = game.config.width / game.config.height;
 
   if (windowRatio < gameRatio) {
-    canvas.style.width = windowWidth + "px";
-    canvas.style.height = (windowWidth / gameRatio) + "px";
-  }
-  else {
-    canvas.style.width = (windowHeight * gameRatio) + "px";
-    canvas.style.height = windowHeight + "px";
+    canvas.style.width = windowWidth + 'px';
+    canvas.style.height = windowWidth / gameRatio + 'px';
+  } else {
+    canvas.style.width = windowHeight * gameRatio + 'px';
+    canvas.style.height = windowHeight + 'px';
   }
 }
-window.onload = function () {
+window.onload = function() {
   resize();
-  window.addEventListener("resize", resize, false);
-}
+  window.addEventListener('resize', resize, false);
+};
