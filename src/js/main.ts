55810af8059e91525567818f144f99bca2358273
Vignesh S <s.vignesh@partner.samsung.com>
2018-09-04 20:44:01 +0530

    Custom window scaler added

--- src/js/main.ts
+++ src/js/main.ts
@@ -43,10 +43,32 @@ gameScene.create = function() {
   // this.add.sprite(0, 0, 'treasure');
 };
 
-new Game({
+let game = new Game({
   width: 640,
   height: 360,
   type: Phaser.AUTO,
   scene: gameScene,
   title: 'Treasure Hunt'
 });
+
+
+function resize() {
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = game.config.width / game.config.height;
+
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = (windowWidth / gameRatio) + "px";
+  }
+  else {
+    canvas.style.width = (windowHeight * gameRatio) + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
+window.onload = function () {
+  resize();
+  window.addEventListener("resize", resize, false);
+}
